e621aa3688dcae1aa160ede5ced5d074e8786b00
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-09-24 09:29:30 +0700

    add a.js, test syntax

---
+++ a.js
@@ -0,0 +1,28 @@
+// deliberately badly formatted + with some eslint issues
+const nums = [1, 2, 3, 4, 5];
+
+function double(n) {
+  return n * 2;
+}
+
+const result = nums.map((num) => {
+  if (num % 2 === 0) {
+    return double(num);
+  } else {
+    console.log("odd number: " + num);
+    return num;
+  }
+});
+
+class Greeter {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHello() {
+    return "Hello, " + this.name;
+  }
+}
+
+let g = new Greeter("World");
+console.log(g.sayHello());
+console.log(result);
