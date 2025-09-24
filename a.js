1a842080e791a058028585b3c6c6dfba31eb6556
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-09-24 10:00:51 +0700

    fix a

--- a.js
+++ a.js
@@ -2,27 +2,29 @@
 const nums = [1, 2, 3, 4, 5];
 
 function double(n) {
-  return n * 2;
+	return n * 2;
 }
 
 const result = nums.map((num) => {
-  if (num % 2 === 0) {
-    return double(num);
-  } else {
-    console.log("odd number: " + num);
-    return num;
-  }
+	if (num % 2 === 0) {
+		return double(num);
+	} else {
+		console.log("odd number: " + num);
+		return num;
+	}
 });
 
 class Greeter {
-  constructor(name) {
-    this.name = name;
-  }
-  sayHello() {
-    return "Hello, " + this.name;
-  }
+	constructor(name) {
+		this.name = name;
+	}
+	sayHello() {
+		return "Hello, " + this.name;
+	}
 }
 
 let g = new Greeter("World");
 console.log(g.sayHello());
 console.log(result);
+
+console.log("Modified Change");
