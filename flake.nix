7844fc5d36994be7df946a0803f72e920eb4f55f
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-09-24 10:01:00 +0700

    env setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "for software labs i guessed ?";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs_22
+
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            nodePackages.prettier
+            nodePackages.eslint
+          ];
+        };
+      }
+    );
+}
